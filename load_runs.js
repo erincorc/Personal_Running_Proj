e0dd4f776da082feac9637b2330c4d6dec1ac8b9
Erin Corcoran <erincorc@gmail.com>
2022-05-11 16:24:09 -0400

    Update load_runs.js

--- load_runs.js
+++ load_runs.js
@@ -3,16 +3,19 @@ const margin = ({top: 20, right: 35, bottom: 20, left: 40})
 fetch("my_runs_20220511.json")
   .then(response => response.json())
   .then(data => {
-    attractions = data;
-    console.log("my_runs_20220511", attractions);}
+    runs = data;
+    console.log("my_runs_20220511", runs);
+
+    for(x=0; x < length(data); x++ ) {
+        var dist_in_mi = data['distance'][x] / 1609;
+        data['dist_in_mi'][x] = dist_in_mi;
+     };
+    
+    console.log("dist_in_mi ",data['dist_in_mi'].slice(0,5));
+
+}
 )
 
-for(x=0; x < length(data); x++ ) {
-    var dist_in_mi = data['distance'][x] / 1609;
-    data['dist_in_mi'][x] = dist_in_mi;
- }
-
-console.log("dist_in_mi ",data['dist_in_mi'].slice(0,5))
 
 d3.csv('my_runs_20220511.csv', d3.autoType).then( data => {
 
