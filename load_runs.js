3c4a9610355b7bbaae18c86be82fadc7bab5c904
Erin Corcoran <erincorc@gmail.com>
2022-05-11 13:47:02 -0400

    Update load_runs.js

--- load_runs.js
+++ load_runs.js
@@ -2,7 +2,7 @@
 var runs = JSON.parse(my_runs_20220501);
 window.print(runs);
 
-document.getElementById('data') = runs;
+document.getElementById('data').textContent = JSON.stringify(runs);
 /*fetch("my_runs_20220511.json")
 .then(response => response.json())
 .then(data => {
