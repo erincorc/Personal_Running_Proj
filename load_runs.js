e641882bd25603bf29b9a2c835c618f77d5c4e63
Erin Corcoran <erincorc@gmail.com>
2022-05-11 13:44:59 -0400

    Added json data and js file

---
+++ load_runs.js
@@ -0,0 +1,11 @@
+
+var runs = JSON.parse(my_runs_20220501);
+window.print(runs);
+
+document.getElementById('data') = runs;
+/*fetch("my_runs_20220511.json")
+.then(response => response.json())
+.then(data => {
+    runs = data;
+    print(runs);
+})*/
