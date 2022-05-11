9788d1379e4180ba99a0472169935c43d413832b
Erin Corcoran <erincorc@gmail.com>
2022-05-11 14:08:13 -0400

    updates to load_run.js

--- load_runs.js
+++ load_runs.js
@@ -1,9 +1,20 @@
 
+/*const jsonData= require('my_runs_20220511.json'); 
+console.log(jsonData); */
+
+fetch("my_runs_20220511.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));
+
+
+/*
 var runs = JSON.parse(my_runs_20220501);
 window.print(runs);
 
 document.getElementById('data').textContent = JSON.stringify(runs);
-/*fetch("my_runs_20220511.json")
+fetch("my_runs_20220511.json")
 .then(response => response.json())
 .then(data => {
     runs = data;
